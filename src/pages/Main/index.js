879d3b9e72350d0a4cb95159a0871354e7b0183f
Daniel de Souza Neri <danielneris01@gmail.com>
2019-07-19 23:57:12 -0300

    i dont know

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,13 +1,37 @@
 import React from 'react';
 
 import Header from '../../components/Header';
+import Tabs from '../../components/Tabs';
+import Menu from '../../components/Menu';
 
-import { Container } from './styles';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import { Container, Content, Card, CardHeader, CardContent, CardFooter, Title, Description, Annotation } from './styles';
 
 export default function Main() {
   return (
     <Container>
       <Header />
+        <Content>
+          <Menu />
+
+          <Card>
+              <CardHeader>
+                  <Icon name="attach-money" size={28} color="#666" />
+                  <Icon name="visibility-off" size={28} color="#666" />
+              </CardHeader>
+              <CardContent>
+                  <Title>Saldo disponível</Title>
+                  <Description>R$ 302.133,20</Description>
+              </CardContent>
+              <CardFooter>
+                  <Annotation>
+                      Transferência de R$ 20,00 recebida de Daniel de Souza Neris hoje às 12:00h
+                  </Annotation>
+              </CardFooter>
+          </Card>
+        </Content>
+      <Tabs />
     </Container>
   );
 }
